19d04ac2027e26f60e6014c2c3f530c9d1a74b58
Rick(Rugui) Chen <rickruguichen@google.com>
2024-01-03 10:43:15 -0500

    update region

--- app-infra/deploy.sh
+++ app-infra/deploy.sh
@@ -1,6 +1,6 @@
 
 export TF_VAR_project_id=$(gcloud config get project)
-export TF_VAR_region=us-west1
+export TF_VAR_region=us-central1
 export TF_VAR_weather_api_key=${WEATHER_API_KEY}
 export TF_VAR_weather_api_url=https://api.openweathermap.org/data/2.5/weather
 
